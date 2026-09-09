b2abd550c09af1623591ec56348fcb727f4dc6f7
dileepcloud06-max <dileepcloud06@gmail.com>
2026-09-10 00:14:52 +0530

    Cache Issue Fixing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,38 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
+import {
+  provideHttpClient,
+  withFetch
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import {
+  provideClientHydration,
+  withEventReplay
+} from '@angular/platform-browser';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
+
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
+
     provideRouter(routes),
+
     provideHttpClient(withFetch()),
-    provideClientHydration(withEventReplay())
+
+    provideClientHydration(
+      withEventReplay()
+    )
   ]
 };
